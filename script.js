f5f63471aeee18ebf1e5fa5ced80e58cdf3f95e1
Pritshu Panda <30794978+DetectiveSQL@users.noreply.github.com>
2023-11-25 00:45:38 +0530

    Added player choice, computer choice, win conditions and play round

--- script.js
+++ script.js
@@ -8,8 +8,18 @@ function computerChoice(){
     const rpsarray = ['rock', 'paper', 'scissor']
     const computerChoice = Math.floor(Math.random()*rpsarray.length)
     console.log("Computer chose: ",rpsarray[computerChoice])
-    return computerChoice
+    return rpsarray[computerChoice]
 }
 
-playerChoice()
-computerChoice()
+function winCondition(playerSelection, computerSelection){
+    const winCondition = {rock:'scissor', scissor:'paper', paper:'rock'};
+
+    return winCondition[playerSelection]===computerSelection ? console.log('You win!'):console.log('Computer wins!')
+}
+
+function playRound(){
+    let playerSelection = playerChoice()
+    return !['rock', 'paper', 'scissor'].includes(playerSelection) ? alert('Invalid Choice'):winCondition(playerSelection,computerChoice())
+}
+
+playRound()
