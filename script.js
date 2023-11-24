c43bcde97fe5b84631f948ab532a76370a28e62b
Pritshu Panda <30794978+DetectiveSQL@users.noreply.github.com>
2023-11-25 01:57:46 +0530

    Added exeption handling for invalid choice

--- script.js
+++ script.js
@@ -14,12 +14,26 @@ function computerChoice(){
 function winCondition(playerSelection, computerSelection){
     const winCondition = {rock:'scissor', scissor:'paper', paper:'rock'};
 
-    return winCondition[playerSelection]===computerSelection ? console.log('You win!'):console.log('Computer wins!')
+    return playerSelection===computerSelection ? console.log('Tie'):winCondition[playerSelection]===computerSelection ? console.log('You win!'):console.log('Computer wins!')
 }
 
 function playRound(){
     let playerSelection = playerChoice()
-    return !['rock', 'paper', 'scissor'].includes(playerSelection) ? alert('Invalid Choice'):winCondition(playerSelection,computerChoice())
+    return !['rock', 'paper', 'scissor'].includes(playerSelection) ? 0:winCondition(playerSelection,computerChoice())
 }
 
-playRound()
+function game(){
+    for (let i=1;i<=5;i++){
+        while(true){
+            let result = playRound()
+            if (result===0){
+                alert('Invalid choice')
+            }
+            else{
+                break;
+            }
+        }
+    }
+}
+
+game()
